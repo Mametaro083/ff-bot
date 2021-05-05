8547f6d13b6960ec1fd42ec735c748edbffeb714
Mametaro083 <81414679+Mametaro083@users.noreply.github.com>
2021-05-05 23:02:25 +0900

    Update avatar.js

--- modules/avatar.js
+++ modules/avatar.js
@@ -3,17 +3,21 @@ const discord = require('discord.js')
 module.exports = avatar
 
 async function avatar(client , message , args) {
-  const embed = new discord.MessageEmbed()
-  .setColor('PURPLE')
-  .setTitle('image link')
-  .setFooter(`Requested by ${message.author.tag}` , message.author.avatarURL())
-  if (!args[0]) return message.channel.send(embed
-  .setURL(message.author.avatarURL())
-  .setImage(message.author.avatarURL())
-  )
-  const member = await client.users.fetch(args[0])
-  message.channel.send(embed
-  .setURL(member.avatarURL())
-  .setImage(member.avatarURL())
-  )
+  try {
+    const embed = new discord.MessageEmbed()
+    .setTitle('image link')
+    .setFooter(`Requested by ${message.author.tag}` , message.avatar.avatarURL())
+    if (!args[0]) return message.channel.send(embed
+    .setURL(message.author.avatarURL())
+    .setImage(message.author.avatarURL())
+    )
+    const member = await client.users.fetch(args[0])
+    message.channel.send(embed
+    .setURL(member.avatarURL())
+    .setImage(member.avatarURL())
+    )
+  } catch(e) {
+    console.error(e)
+    message.channel.send(codeFunc(e))
+  }
 }
